fa4f7ca35b5f79915f981d066099860d53b0126b
John Uberbacher <johnuberbacher@gmail.com>
2023-04-07 01:25:18 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # GitHub user to monitor
 github_user = 'github_username'
 
-# Set up the Discord channel to interact with
+# Set up the Discord channels to interact with 
 channel_id = discord_channel_id
+game_sales_channel_id = game_sales_channel_id
+anime_channel_id = anime_channel_id
 bot_token = 'discord_bot_token'
