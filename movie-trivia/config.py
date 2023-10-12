8512bc461516ab0b8cbdd700195bd010c57f16f0
johnuberbacher <johnuberbacher@gmail.com>
2023-10-11 22:27:46 -0600

    update movie trivia script

--- movie-trivia/config.py
+++ movie-trivia/config.py
@@ -1,2 +1,2 @@
 bot_token = 'abc' # replace with bot_id as a string
-movie_channel_id = 123 # replace with channel_id as a number
+channel_id = 123 # replace with channel_id as a number
--- movie-trivia/script.py
+++ movie-trivia/script.py
@@ -2,8 +2,9 @@ import discord
 import requests
 import asyncio
 import html
+import random
 
-from config import movie_channel_id, bot_token
+from config import channel_id, bot_token
 
 intents = discord.Intents.default()
 intents.members = True
@@ -23,15 +24,32 @@ async def get_trivia():
     if response.status_code == 200:
         data = response.json()
         question = html.unescape(data['results'][0]['question'])
-        answer = html.unescape(data['results'][0]['correct_answer'])
+        correct_answer = html.unescape(data['results'][0]['correct_answer'])
+        incorrect_answers = [html.unescape(answer) for answer in data['results'][0]['incorrect_answers']]
+        
+        options = [correct_answer] + incorrect_answers
+        random.shuffle(options)
+        
+        # Format the options with a newline after each option
+        options_text = '\n'.join(f"{option}" for option in options)
+        
+        # Check if correct_answer is smaller than 10 characters
+        if len(correct_answer) < 20:
+            # Add spaces around correct_answer
+            correct_answer = f"{' ' * 8}{correct_answer}{' ' * 8}"
 
-        message = "**Random Movie Trivia:**\n\n" + question + '\n\nThe answer is ||' + answer + '||!\nWant another? Just type `/trivia` in the movie channel.'
-        print(message)
+            
+        channel = client.get_channel(channel_id)
+        embed = discord.Embed(
+            colour=0x00b0f4,
+            color=discord.Color.dark_theme()
+        )
+        embed.set_author(name="Random Movie Trivia")
+        embed.add_field(name="", value=f"{question}\n\n{options_text}", inline=False)
+        embed.add_field(name="", value=f"The answer is ||{correct_answer}||!", inline=False)
 
-        channel = client.get_channel(movie_channel_id)
-        await channel.send(message)
+        await channel.send(embed=embed)
         await client.close()
-        await quit()
     else:
         print("Request failed with status code:", response.status_code)
 
